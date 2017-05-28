e0199231a8cc07f92100d232aba74bb037724a96
Lawton Spelliscy <lawton@tribalscale.com>
2017-05-28 15:14:55 -0400

    set code to initial state

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-// import { Math2 as math } from '../utils/math';
-import { Math4 as math } from '../utils/math';
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { equation: '', calculatedResult: '' };
-    ['handleValueChange', 'calculateValue', 'calculateValue2'].forEach(
+    this.state = { equation: ''};
+    ['handleValueChange'].forEach(
       (method) => {
         this[method] = this[method].bind(this);
       }
@@ -17,42 +15,15 @@ export default class Calculator extends Component {
     this.setState({ equation: event.target.value });
   }
 
-  calculateValue2() {
-    const additionSum = math.parseEquation(this.state.equation);
-    if (isNaN(additionSum)) {
-      this.setState(
-        {
-          calculatedResult: '',
-          errorMessage: 'Invalid input. Please input only numbers.'
-        }
-      );
-    } else {
-      this.setState(
-        {
-          calculatedResult: additionSum,
-          errorMessage: ''
-        }
-    );
-    }
-  }
-
-  calculateValue() {
-    const additionSum = math.parseEquation(this.state.equation);
-    this.setState({calculatedResult: additionSum});
-  }
-
-
   render() {
     return (
       <div>
         <input
           value={this.state.equation}
           onChange={this.handleValueChange} />
-        <button onClick={this.calculateValue2}>
+        <button>
               =
         </button>
-        <label className="result">{this.state.calculatedResult}</label>
-        <div className="error-message">{this.state.errorMessage}</div>
       </div>
     );
   }
